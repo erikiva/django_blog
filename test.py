3760d2b62d0cbf4f0bea9357322e69a989cb49b3
natalia <nataliav@gmail.com>
2015-09-13 14:58:21 +0200

    My Django First app, first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+print('Hello, Django girls!')
+
+if 5 > 2:
+  print('5 is indeed greater than 2')
+else:
+  print('5 is not greater than 2')
+
+def hi():
+  print('Hi there!')
+  print('How are you?')
+
+hi()
